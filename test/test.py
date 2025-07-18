ea1315b4186deaefb9a382c76a04056f11b38c26
Islam-Ihfaz <144639167+Islam-Ihfaz@users.noreply.github.com>
2025-07-18 12:13:09 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, RisingEdge, Timer
 
 
 @cocotb.test()
@@ -26,12 +26,37 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
+    dut.rst_n.value = 0
+    dut.ui_in[0].value = 0 # x1=0
+    
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 5)
 
+    dut.rst_n.value = 1
+
+    await RisingEdge(clk)
+
+    dut.ui_in[0].value = 0 # x1=0
+    dut.ui_in[0].value = 1 # x1=1
+    dut.ui_in[0].value = 0 # x1=0
+    dut.ui_in[0].value = 1 # x1=1
+    dut.ui_in[0].value = 0 # x1=0
+    dut.ui_in[0].value = 1 # x1=1
+    dut.ui_in[0].value = 0 # x1=0
+    dut.ui_in[0].value = 1 # x1=1
+    dut.ui_in[0].value = 0 # x1=0
+    dut.ui_in[0].value = 1 # x1=1
+    dut.ui_in[0].value = 0 # x1=0
+    dut.ui_in[0].value = 1 # x1=1
+    dut.ui_in[0].value = 0 # x1=0
+    dut.ui_in[0].value = 1 # x1=1
+    dut.ui_in[0].value = 0 # x1=0
+    dut.ui_in[0].value = 1 # x1=1
+    dut.ui_in[0].value = 0 # x1=0
+
+    await ClockCycles(dut.clk, 10)
+
+    
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
     assert dut.uo_out.value == 50
